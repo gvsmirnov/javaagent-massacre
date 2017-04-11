e931743f1905c6b5ab25e2b53fc958be2e452bd5
Gleb Smirnov <me@gvsmirnov.ru>
2017-04-11 17:27:26 +0300

    Run results + better scripts

--- dissect.sh
+++ dissect.sh
@@ -15,19 +15,22 @@ JAR_FILE=build/libs/massacre-$EXPERIMENT_ID.jar
 
 START=`date +%s`
 
-java -verbose:gc -XX:+PrintGCTimeStamps -Xloggc:exp.$EXPERIMENT_ID.noa.gc.log -Djavaagent:$JAR_FILE -jar $JAR_FILE &
+java -XX:NativeMemoryTracking=summary -javaagent:$JAR_FILE -jar $JAR_FILE &
 
 JAVA_PID=$!
 
 trap "kill -9 $JAVA_PID" EXIT
 
-for i in `seq 1 10`; do
-  RSS=`ps -o rss -p $JAVA_PID | grep -v RSS`
-  NOW=`date +%s`
-  T=`expr $NOW - $START`
+for i in `seq 1 60`; do
+  output_file=exp.$EXPERIMENT_ID.null.$i.txt
+  jcmd $JAVA_PID VM.native_memory summary > $output_file
 
-  printf "T+%05sT: RSS=%010sK" "$T" "$RSS"
-  echo ""
+  T=`expr $(date +%s) - $START`
+  RSS=`ps -o rss -p $JAVA_PID | grep -v RSS`
+  VMS=`grep 'Total:' $output_file`
+
+  printf "T+%05dT: RSS=%010dK; JVM=%s;" "$T" "$RSS" "$VMS"
+  echo "" # line break
 
   sleep 1
 done
--- src/main/java/ru/jug/gvsmirnov/javaagent/massacre1/NoopAgent.java
+++ src/main/java/ru/jug/gvsmirnov/javaagent/massacre1/NoopAgent.java
@@ -11,7 +11,27 @@ public class NoopAgent {
 
     private static byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
                                     ProtectionDomain protectionDomain, byte[] classfileBuffer) {
-        return classfileBuffer;
+        return null;
     }
+
+    /*
+        No agent:
+        Total: reserved=42,110,983KB, committed=36,722,415KB
+            Internal (reserved=17,804KB,    committed=17,804KB)
+
+        With Agent:
+        Total: reserved=51,311,858KB, committed=47,826,782KB
+            Internal (reserved=21,352,318KB, committed=21,352,318KB)
+
+        With Agent (no retransformation):
+        Total: reserved=39,430,385KB, committed=34,997,789KB
+            Internal (reserved=17,247KB,    committed=17,247KB)
+
+        With Agent (retransformation, return null)
+        Total: reserved=39446335KB, committed=35548543KB
+            Internal (reserved=17250KB, committed=17250KB)
+
+        // TODO: figure out the crazy numbers of virtual memory usage. Why no swapping? Why commit and not use?
+     */
 
 }
